c44b3d2ff997b3eea7b96298905e309eba9dae21
Dave Sexton <RxDave@users.noreply.github.com>
2018-05-14 14:01:07 -0400

    Lab for connecting (WIP)

--- Main/Testing/Dank.Discord.Labs/Program.cs
+++ Main/Testing/Dank.Discord.Labs/Program.cs
@@ -1,11 +1,39 @@
-﻿namespace Dank.Discord.Labs
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Dank.Discord.Labs
 {
   class Program
   {
+    private static readonly string InfoFile = @"..\..\Properties\PrivateDiscordInfo.txt";
+    private static readonly HttpClient Client = new HttpClient();
+
     static void Main(string[] args)
     {
-      // merge this
-      //No you merge this
+      var response = ConnectAsync(Initialize()).Result;
+
+      Console.WriteLine(response);
+      Console.ReadKey();
+    }
+
+    private static async Task<string> ConnectAsync(ConnectionInfo connection)
+    {
+      var response = await Client.GetAsync(connection.Url).ConfigureAwait(false);
+      var content = await response.Content.ReadAsStringAsync();
+
+      return response.IsSuccessStatusCode ? content : "FAILED: " + response.StatusCode + Environment.NewLine + content;
+    }
+
+    private static ConnectionInfo Initialize()
+    {
+      var lines = File.ReadAllLines(InfoFile);
+      var url = lines[0];
+      var clientId = lines[1];
+      var secret = lines[2];
+
+      return new ConnectionInfo(new Uri(url), clientId, secret);
     }
   }
 }

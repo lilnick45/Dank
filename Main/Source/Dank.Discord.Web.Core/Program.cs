be04941a3888d751436e9644ced51ffba795cb55
Dave Sexton <RxDave@users.noreply.github.com>
2018-05-16 16:21:59 -0400

    Implemented Dank connection in the web API

--- Main/Source/Dank.Discord.Web.Core/Program.cs
+++ Main/Source/Dank.Discord.Web.Core/Program.cs
@@ -1,25 +1,67 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reactive;
+using System.Reactive.Linq;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace Dank.Discord.Web.Core
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            BuildWebHost(args).Run();
-        }
+  public class Program
+  {
+    private const string DankUserId = "445634069439578132";
 
-        public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .Build();
+    private static readonly string infoFile = @"$pdi";
+    private static readonly string authToken = File.ReadAllLines(infoFile)[0];
+
+    private static DiscordClient client;
+
+    public static void Main(string[] args)
+    {
+      using (client = new DiscordClient(DankUserId, authToken))
+      {
+        using (client.Events.Subscribe(
+          next => Log.Info("RECEIVED: " + next),
+          ex => Log.Info(ex),
+          () => Log.Info("DONE")))
+        using ((from message in client.UserMessages
+                let response = TryProcessCommand(message.Content)
+                where response != null
+                from _ in client.PostMessageAsync(message.ChannelId, response)
+                select Unit.Default)
+                .Subscribe())
+        {
+          client.Connect();
+
+          WebHost.CreateDefaultBuilder(args)
+                 .UseStartup<Startup>()
+                 .Build()
+                 .Run();
+        }
+      }
     }
+
+    private static string TryProcessCommand(string content)
+    {
+      if (!string.IsNullOrEmpty(content))
+      {
+        var parts = content.Split();
+
+        if (parts.Length >= 2 && parts[0] == "/dank")
+        {
+          var command = parts[1];
+
+          switch (command)
+          {
+            case "help":
+            default:
+              return "Don't worry, **Dank** commands are coming soon!";
+          }
+        }
+      }
+
+      return null;
+    }
+  }
 }
